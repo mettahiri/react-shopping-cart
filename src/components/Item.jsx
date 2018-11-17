039f3caaa96c209d0be210b29b0debd9cef7cd68
m.ettahiri <m.ettahiri18@gmail.com>
2018-11-17 19:27:28 +0100

    init

---
+++ src/components/Item.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Item extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Item1</h1>
+         );
+    }
+}
+ 
+export default Item;
---
+++ src/components/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class ShoppingCart extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <Items />
+         );
+    }
+}
+ 
+export default ShoppingCart;
